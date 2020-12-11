68cc6f5c20a95d4daf2d06d85f881f55927df187
akrjuger <krjuger@ya.ru>
2020-12-11 06:06:53 +0300

    add const with HttpCodes

--- src/consts.js
+++ src/consts.js
@@ -34,8 +34,24 @@ const DEFAULT_COMMAND = `--help`;
  */
 const USER_ARGV_INDEX = 2;
 
+
+/**
+ * Коды для ответов сервера
+ *
+ * @const
+ * @type {Object}
+ */
+const HttpCode = {
+  OK: 200,
+  NOT_FOUND: 404,
+  INTERNAL_SERVER_ERROR: 500,
+  FORBIDDEN: 403,
+  UNAUTHORIZED: 401,
+};
+
 module.exports = {
   DEFAULT_COMMAND,
   ExitCodes,
-  USER_ARGV_INDEX
+  USER_ARGV_INDEX,
+  HttpCode
 };
