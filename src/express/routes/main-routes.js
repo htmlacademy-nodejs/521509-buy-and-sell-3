2c8bec08a6636011cd226cc67c771533328ac32e
akrjuger <krjuger@ya.ru>
2020-12-13 07:55:07 +0300

    add login page

--- src/express/routes/main-routes.js
+++ src/express/routes/main-routes.js
@@ -12,7 +12,7 @@ const mainRoutes = new Router();
 
 mainRoutes.get(`/`, (req, res) => res.send(`/`));
 mainRoutes.get(`/register`, (req, res) => res.send(`/register`));
-mainRoutes.get(`/login`, (req, res) => res.send(`/login`));
+mainRoutes.get(`/login`, (req, res) => res.render(`pages/login`));
 mainRoutes.get(`/search`, (req, res) => res.send(`/search`));
 
 module.exports = mainRoutes;
