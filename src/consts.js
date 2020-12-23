9ecac8a2e09cf1d42dbfc61bb60a07aba261e419
akrjuger <krjuger@ya.ru>
2020-12-23 08:07:28 +0300

    add logging configuration

--- src/consts.js
+++ src/consts.js
@@ -68,11 +68,23 @@ const MAX_ID_LENGTH = 5;
  */
 const API_PREFIX = `/api`;
 
+
+/**
+ * Переменные окружения. Нужно будет перенести в .env
+ * @const
+ * @type {Object}
+ */
+const Env = {
+  DEVELOPMENT: `development`,
+  PRODUCTION: `production`
+};
+
 module.exports = {
   DEFAULT_COMMAND,
   ExitCodes,
   USER_ARGV_INDEX,
   HttpCode,
   MAX_ID_LENGTH,
-  API_PREFIX
+  API_PREFIX,
+  Env
 };
