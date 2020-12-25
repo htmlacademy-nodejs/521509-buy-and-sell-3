4b27a78151e9a73066ae7a1faf649fac814fafb1
akrjuger <krjuger@ya.ru>
2020-12-25 07:40:40 +0300

    add my rendering and my comments rendering

--- src/express/routes/my-routes.js
+++ src/express/routes/my-routes.js
@@ -8,9 +8,19 @@
 
 const {Router} = require(`express`);
 
-const myRoutes = new Router();
+const API = require(`../api`);
 
-myRoutes.get(`/`, (req, res) => res.render(`pages/my/my-tickets`));
-myRoutes.get(`/comments`, (req, res) => res.render(`pages/my/comments`));
+const myRoutes = new Router();
+const api = API.getDefaultAPI();
+
+myRoutes.get(`/`, async (req, res) => {
+  // pug 3.0.0 имеет баг с переменными, которые начинаются с `of`. https://github.com/pugjs/pug/issues/3263/ Переменную offer использовать нельзя
+  const adverts = await api.getOffers();
+  res.render(`pages/my/my-tickets`, {adverts});
+});
+myRoutes.get(`/comments`, async (req, res) => {
+  const adverts = await api.getOffers();
+  res.render(`pages/my/comments`, {adverts: adverts.slice(0, 3)});
+});
 
 module.exports = myRoutes;
