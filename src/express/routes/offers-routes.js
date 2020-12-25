2389937cedcee618908fde7f4c65e85be6db76e6
akrjuger <krjuger@ya.ru>
2020-12-25 09:13:06 +0300

    add new offer creation

--- src/express/routes/offers-routes.js
+++ src/express/routes/offers-routes.js
@@ -6,7 +6,25 @@
  * @module /src/express/routes/offers-routes
  */
 
+const path = require(`path`);
+
 const {Router} = require(`express`);
+const {nanoid} = require(`nanoid`);
+const multer = require(`multer`);
+
+const UPLOAD_DIR = `../upload/img/`;
+
+const uploadDirAbsolute = path.resolve(__dirname, UPLOAD_DIR);
+
+const storage = multer.diskStorage({
+  destination: uploadDirAbsolute,
+  filename: (req, file, cb) => {
+    const uniqueName = nanoid(10);
+    const extension = file.originalname.split(`.`).pop();
+    cb(null, `${uniqueName}.${extension}`);
+  }
+});
+const upload = multer({storage});
 
 const API = require(`../api`);
 
@@ -15,6 +33,25 @@ const offersRoutes = new Router();
 const api = API.getDefaultAPI();
 
 offersRoutes.get(`/add`, (req, res) => res.render(`pages/offers/new-ticket`));
+
+offersRoutes.post(`/add`, upload.single(`avatar`), async (req, res) => {
+  const {body, file} = req;
+  const offerData = {
+    picture: file.filename,
+    sum: body.price,
+    type: body.action,
+    description: body.comment,
+    title: body[`ticket-name`],
+    category: [body.category]
+  };
+  try {
+    await api.createOffer(offerData);
+    res.redirect(`/my`);
+  } catch (e) {
+    res.redirect(`back`);
+  }
+});
+
 offersRoutes.get(`/:id`, (req, res) => res.render(`pages/offers/ticket`));
 offersRoutes.get(`/edit/:id`, async (req, res) => {
   const [advert, categories] = await Promise.all([api.getOffer(req.params.id), api.getCategories()]);
