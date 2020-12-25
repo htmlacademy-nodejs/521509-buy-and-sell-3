882e2020d125b5ebbe45334a7665b9d219ff52ad
akrjuger <krjuger@ya.ru>
2020-12-25 07:59:47 +0300

    add offer editing rendering

--- src/express/routes/offers-routes.js
+++ src/express/routes/offers-routes.js
@@ -8,12 +8,18 @@
 
 const {Router} = require(`express`);
 
+const API = require(`../api`);
+
 const offersRoutes = new Router();
 
+const api = API.getDefaultAPI();
 
 offersRoutes.get(`/add`, (req, res) => res.render(`pages/offers/new-ticket`));
 offersRoutes.get(`/:id`, (req, res) => res.render(`pages/offers/ticket`));
-offersRoutes.get(`/edit/:id`, (req, res) => res.render(`pages/offers/ticket-edit`));
+offersRoutes.get(`/edit/:id`, async (req, res) => {
+  const [advert, categories] = await Promise.all([api.getOffer(req.params.id), api.getCategories()]);
+  res.render(`pages/offers/ticket-edit`, {advert, categories});
+});
 
 offersRoutes.get(`/category/:id`, (req, res) => res.render(`pages/offers/category`));
 
