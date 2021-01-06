f619fb11a2b6df7d94181a25689cea200b8de3fd
akrjuger <krjuger@ya.ru>
2021-01-06 21:24:09 +0300

    some query optimisation

--- queries.sql
+++ queries.sql
@@ -13,25 +13,35 @@ SELECT categories.id, categories.title, count(categories.id) FROM offers_categor
   ORDER BY count(categories.id) DESC;
 
 -- Получить список объявлений (идентификатор объявления, заголовок объявления, стоимость, тип объявления, текст объявления, дата публикации, имя и фамилия автора, контактный email, количество комментариев, наименование категорий). Сначала свежие объявления;
+
+-- Спасибо наставнику за наводку на курс по SQL, создаем представление, чтобы не повторять этот код при необходимости выборки по id или иным полям.
+DROP VIEW IF EXISTS offers_info;
+CREATE VIEW offers_info AS(
 SELECT
-  offers.id AS "Offer id",
-  offers.title AS "Offer title",
-  offers.cost AS "Offer Cost",
-  offers_types.title AS "Offer type",
-  offers.description AS "Offer text",
-  offers.creation_date AS "Offer CD",
-  users.first_name AS "User first name",
-  users.last_name AS "User last name",
-  users.email AS "User email",
-  count(comments.offer_id) AS "Comments count",
-  offers_categories.categories_titles AS "Categories"
+  offers.id AS "id",
+  offers.title AS "title",
+  offers.cost AS "cost",
+  offers_types.title AS "type",
+  offers.description AS "text",
+  offers.creation_date AS "creation_date",
+  users.first_name AS "user_first_name",
+  users.last_name AS "user_last_name",
+  users.email AS "user_email",
+  comments.comments_count AS "comments_count",
+  offers_categories.categories_titles AS "categories"
     FROM offers
   INNER JOIN offers_types
     ON offers.type_id = offers_types.id
   INNER JOIN users
     ON offers.user_id = users.id
-  LEFT JOIN comments
-    ON comments.offer_id = offers.id
+  LEFT JOIN (
+    SELECT
+      comments.offer_id,
+      count(comments.offer_id) AS "comments_count"
+    FROM comments
+    GROUP BY comments.offer_id
+  )
+    comments ON comments.offer_id = offers.id
   INNER JOIN
     (
       SELECT offers_categories.offer_id, string_agg(categories.title, ', ') AS "categories_titles"
@@ -40,43 +50,14 @@ SELECT
       GROUP BY offers_categories.offer_id
     )
      offers_categories ON offers_categories.offer_id = offers.id
-  GROUP BY offers.id, offers_types.id, users.id, offers_categories.categories_titles
   ORDER BY offers.creation_date DESC
-    ;
-
+    );
+-- выбираем наше новое представление
+SELECT * FROM offers_info;
 
 -- Получить полную информацию определённого объявления (идентификатор объявления, заголовок объявления, стоимость, тип объявления, текст объявления, дата публикации, имя и фамилия автора, контактный email, количество комментариев, наименование категорий);
 -- Это тот же вопрос, что и выше, только добавилось условие WHERE. В этом запросе ищем объявление с id = 2
-SELECT
-  offers.id AS "Offer id",
-  offers.title AS "Offer title",
-  offers.cost AS "Offer Cost",
-  offers_types.title AS "Offer type",
-  offers.description AS "Offer text",
-  offers.creation_date AS "Offer CD",
-  users.first_name AS "User first name",
-  users.last_name AS "User last name",
-  users.email AS "User email",
-  count(comments.offer_id) AS "Comments count",
-  offers_categories.categories_titles AS "Categories"
-    FROM offers
-  INNER JOIN offers_types
-    ON offers.type_id = offers_types.id
-  INNER JOIN users
-    ON offers.user_id = users.id
-  LEFT JOIN comments
-    ON comments.offer_id = offers.id
-  INNER JOIN
-    (
-      SELECT offers_categories.offer_id, string_agg(categories.title, ', ') AS "categories_titles"
-      FROM offers_categories
-      INNER JOIN categories ON categories.id = offers_categories.category_id
-      GROUP BY offers_categories.offer_id
-    )
-     offers_categories ON offers_categories.offer_id = offers.id
-  WHERE offers.id = 2
-  GROUP BY offers.id, offers_types.id, users.id, offers_categories.categories_titles
-    ;
+SELECT * FROM offers_info WHERE id = 2;
 
 
 -- Получить список из 5 свежих комментариев (идентификатор комментария, идентификатор объявления, имя и фамилия автора, текст комментария);
